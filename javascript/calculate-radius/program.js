eb9d42cea45051c125bc65720d6eb7068c60ee50
Guilherme Castro <guilhermecastro.developer@gmail.com>
2019-08-18 02:03:35 -0300

    first commit

---
+++ javascript/calculate-radius/program.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+let raio = "";
+
+const leitor = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+leitor.question("Informe o valor do raio: ", function(answer){
+  let raio = answer;
+  let area = (3.1416*(Math.pow(raio, 2)))
+  console.log("A área é: " + area);
+  leitor.close();
+})
+
