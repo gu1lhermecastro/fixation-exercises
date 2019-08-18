0c1a0b959a8c312d0ce896ab4d097bad5454e001
Guilherme Castro <guilhermecastro.developer@gmail.com>
2019-08-18 03:17:07 -0300

    Exercicio que calcula lampadas necessarias para iluminar um comodo

---
+++ python/calculate-required-light/program.py
@@ -0,0 +1,8 @@
+potencial = int(input('Digite a potência da lâmpada: '))
+
+width = int(input('Digite a largura do cômodo: '))
+lenght = int(input('Digite o comprimento do cômodo: '))
+
+area = (lenght*width)
+
+print('Você precisará de:', area, 'lâmapadas para iluminar o seu cômodo.')
